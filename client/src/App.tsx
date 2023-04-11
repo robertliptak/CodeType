557413bef629423d525be822514b330705405346
robertliptak <robik.liptak@gmail.com>
2023-04-11 11:35:05 +0200

    ESLint, Prettier and Husky set up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return <div className="App" />;
 }
 
 export default App;
