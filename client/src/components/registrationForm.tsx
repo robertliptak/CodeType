685f4200fdf1039f1bdb842e09faf17c1f716f1d
robertliptak <robik.liptak@gmail.com>
2023-05-09 14:33:33 +0200

    Registration form validation improved

--- client/src/components/registrationForm.tsx
+++ client/src/components/registrationForm.tsx
@@ -3,122 +3,122 @@ import { useFormik } from "formik";
 import { Link } from "react-router-dom";
 import { createUser } from "../api/auth";
 import registrationValidationSchema from "../validation/registrationValidation";
+import { IRegistration, IRegistrationFormErrors } from "../types/auth";
 
 const RegistrationForm: React.FC = () => {
-  const formik = useFormik({
+  const [backendErrorMessage, setBackendErrorMessage] = useState({
+    showError: false,
+    errorContent: null,
+  });
+
+  const formik = useFormik<IRegistration>({
     initialValues: {
       username: "",
       email: "",
       password: "",
       confirmedPassword: "",
     },
+    validate: (values: IRegistration) => {
+      const errors: IRegistrationFormErrors = {};
+
+      if (values.password !== values.confirmedPassword) {
+        errors.confirmedPassword = "Passwords do not match";
+      }
+
+      return errors;
+    },
     validationSchema: registrationValidationSchema,
-    onSubmit: (values) => {
-      console.log(values);
+    onSubmit: async (values) => {
+      try {
+        const user = await createUser(values);
+        setBackendErrorMessage(() => ({
+          showError: false,
+          errorContent: null,
+        }));
+        console.log("User created:", user);
+      } catch (err: any) {
+        const error = err.response.data.message || "Unknown error occurred";
+        console.log(error);
+        setBackendErrorMessage(() => ({
+          showError: true,
+          errorContent: error,
+        }));
+      }
     },
   });
 
-  // const [registrationData, setRegistrationData] = useState({
-  //   username: "",
-  //   email: "",
-  //   password: "",
-  //   confirmedPassword: "",
-  // });
-  // const [errorMessage, setErrorMessage] = useState({
-  //   showError: false,
-  //   errorContent: null,
-  // });
+  const getFieldError = (fieldName: keyof IRegistration) => {
+    const errorMessage =
+      formik.errors[fieldName] && formik.touched[fieldName]
+        ? formik.errors[fieldName]
+        : null;
+    const isError = formik.errors[fieldName] && formik.touched[fieldName];
+    const isValid = !formik.errors[fieldName] && formik.touched[fieldName];
+    let inputClassName = "";
+    if (isError) {
+      inputClassName = "is_error";
+    } else if (isValid) {
+      inputClassName = "is_valid";
+    }
 
-  // const formik.handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setRegistrationData({
-  //     ...registrationData,
-  //     [e.target.name]: e.target.value,
-  //   });
-  // };
-
-  // const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-
-  //   setErrorMessage(() => ({
-  //     showError: false,
-  //     errorContent: null,
-  //   }));
-
-  //   try {
-  //     const user = await createUser(registrationData);
-  //     console.log("User created:", user);
-  //   } catch (err: any) {
-  //     const error = err.response.data.message || "Unknown error occurred";
-  //     console.log(error);
-  //     setErrorMessage(() => ({
-  //       showError: true,
-  //       errorContent: error,
-  //     }));
-  //   }
-  // };
+    return { errorMessage, inputClassName };
+  };
 
   return (
     <div className="auth_container">
       <h1>Create an account</h1>
       <p className="my-4">Please enter your details</p>
       <form onSubmit={formik.handleSubmit}>
-        <p>
-          {formik.errors.username && formik.touched.username
-            ? formik.errors.username
-            : "Username"}
-        </p>
+        <p>{getFieldError("username").errorMessage}</p>
         <input
           type="text"
           placeholder="Username"
           name="username"
-          className="hovno"
+          className={getFieldError("username").inputClassName}
           value={formik.values.username}
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
         />
-        <p>
-          {formik.errors.email && formik.touched.email
-            ? formik.errors.email
-            : "Email"}
-        </p>
+        <p>{getFieldError("email").errorMessage}</p>
         <input
           type="email"
           placeholder="Email"
           name="email"
+          className={getFieldError("email").inputClassName}
           value={formik.values.email}
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
         />
-        <p>
-          {formik.errors.password && formik.touched.password
-            ? formik.errors.password
-            : "Password"}
-        </p>
+        <p>{getFieldError("password").errorMessage}</p>
         <input
           type="password"
           placeholder="Password"
           name="password"
+          className={getFieldError("password").inputClassName}
           value={formik.values.password}
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
         />
-        <p>
-          {formik.errors.confirmedPassword && formik.touched.confirmedPassword
-            ? formik.errors.confirmedPassword
-            : "Confirmad password"}
-        </p>
+        <p>{getFieldError("confirmedPassword").errorMessage}</p>
         <input
           type="password"
           placeholder="Verify password"
           name="confirmedPassword"
+          className={getFieldError("confirmedPassword").inputClassName}
           value={formik.values.confirmedPassword}
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
         />
-        <button type="submit" className="filled_button my-6">
+        <button
+          type="submit"
+          className="filled_button my-6"
+          disabled={formik.isSubmitting || !formik.isValid}
+        >
           Sign up
         </button>
-        {/* {errorMessage.showError && <p>{errorMessage.errorContent}</p>} */}
+        {backendErrorMessage.showError && (
+          <p>{backendErrorMessage.errorContent}</p>
+        )}
         <p>
           Already have an account?{" "}
           <Link className="auth_link" to="/login">
--- client/src/types/auth.ts
+++ client/src/types/auth.ts
@@ -8,3 +8,9 @@ export interface ILogin {
   email: string;
   password: string;
 }
+export interface IRegistrationFormErrors {
+  username?: string;
+  email?: string;
+  password?: string;
+  confirmedPassword?: string;
+}
